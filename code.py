ed629611611b5b9144aced775f773ff92bca76fb
ASJADSYED08 <hr49335@gmail.com>
2021-10-13 00:13:54 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,22 @@
+"""print("enter your num 1")
+inpnum1=input()
+print("enter yournum 2")
+inpnum2=input()
+sum=int(inpnum1)+int(inpnum2)
+print("you entered sum is ",sum)"""
+"""
+var1="asjad"
+print(var1)
+"""
+mystr="SYED ASJAD HUSSAIN RIZVI"
+print(len(mystr))
+#print(mystr[0:13:-1])
+#FUNCTIONS
+print(mystr.startswith("SYED"))
+print(mystr.endswith("RIZVI"))
+print(mystr.count("A"))
+print(mystr.capitalize())
+print(mystr.upper())
+print(mystr.lower())
+print(mystr.find("Y"))
+print(mystr.replace("ASJAD","ANSAR"))
